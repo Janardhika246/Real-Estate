50767799e9fd160571839758507bfd9b135e7ce4
janardhika <janardhika246@gmail.com>
2024-08-21 02:24:57 +0530

    21/08

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from flask import Flask, render_template
+import requests
+import os
 
 app = Flask(__name__)
 
+API_KEY = os.getenv('yDgFEZO0g76kMWiy6ADBnB0AojzF2E')
+
 @app.route('/')
 def home():
-    # You can add dynamic data fetching from an API or database here
     return render_template('index.html')
+
+@app.route('/property')
+def property_page():
+    return render_template('property.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/join')
+def join():
+    return render_template('join.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
